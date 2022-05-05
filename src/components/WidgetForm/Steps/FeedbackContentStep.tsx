0119a511610436f74250e59ca7e9bc4905c665bc
Rodrigo Sobral <rodrigo.sobral@usabit.com.br>
2022-05-05 10:09:48 -0300

    feat(button): add disable button feedback when havent comment

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -15,6 +15,12 @@ export function FeedbackContentStep({
 }: FeedbackTypeContentProps) {
 	const feedbackTypeInfo = feedbackTypes[feedbackType];
 	const [screenshot, setScreenshot] = useState<string | null>(null);
+	const [comment, setComment] = useState("");
+
+	function handleSubmitFeedback(e: FormEvent) {
+		e.preventDefault();
+		console.log("envio de feedback", comment, screenshot);
+	}
 
 	return (
 		<>
@@ -43,6 +49,7 @@ export function FeedbackContentStep({
 				<textarea
 					className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
 					placeholder="Conte com detalhes o que está acontecendo..."
+					onChange={(event) => setComment(event.target.value)}
 				/>
 				<footer className="flex gap-2 mt-2">
 					<ScreenshotButton
@@ -51,7 +58,9 @@ export function FeedbackContentStep({
 					/>
 					<button
 						type="submit"
-						className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+						className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+						disabled={comment.length === 0}
+						onClick={handleSubmitFeedback}
 					>
 						Enviar feedback
 					</button>
